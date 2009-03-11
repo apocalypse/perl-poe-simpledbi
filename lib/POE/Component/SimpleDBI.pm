3ffe63781a418ce59ca02737786b2e00e01fde7a
Apocalypse <apoc@satellite.(none)>
2009-03-11 17:19:16 +0100

    add link to CPAN::Forum

--- lib/POE/Component/SimpleDBI.pm
+++ lib/POE/Component/SimpleDBI.pm
@@ -1975,6 +1975,10 @@ L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=POE-Component-SimpleDBI>
 
 L<http://search.cpan.org/dist/POE-Component-SimpleDBI>
 
+=item * CPAN::Forum
+
+L<http://www.cpanforum.com/dist/POE-Component-SimpleDBI>
+
 =back
 
 =head2 Bugs
